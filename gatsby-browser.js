0cb0be01c37dc1f17119d0dfaac52addffef721b
yiluomyt <mytyiluo@outlook.com>
2019-06-04 13:57:03 +0800

    添加内容更新提示

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,13 @@
 import "./src/styles/global.less"
 import "./src/styles/prism-vs.css"
 import "./src/styles/prism-custom.less"
+
+export const onServiceWorkerUpdateReady = () => {
+  const answer = window.confirm(
+    `博客内容已更新，是否加载新的内容？`
+  )
+
+  if (answer === true) {
+    window.location.reload()
+  }
+}
